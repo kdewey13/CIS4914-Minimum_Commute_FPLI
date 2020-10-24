339c0e1527238d7ebc1f838f5b7f5b9a4353444e
kdewey13 <kdewey@ufl.edu>
2020-10-24 01:14:33 -0400

    finished straight line distance calc and store

--- minimum_commute_calculator.py
+++ minimum_commute_calculator.py
@@ -19,13 +19,13 @@ if connection is not None:
     try:
         cursor = connection.cursor()
         # create a list of all the school data information
-        school_list = cursor.execute('Select * FROM school_info')
+        school_list = list(cursor.execute('Select * FROM school_info').fetchall())
     except Error as e:
         print(e)
 
 # get the indices that the values of interest are at (if the input file is in the correct order this should
 # be the same each time, but this doesn't hurt anything to check)
-id_index, lat_index, long_index, level_index = '', '', '', ''
+id_index, lat_index, long_index, level_index, district_index = '', '', '', '', ''
 for index in range(0, len(cursor.description)):
     if cursor.description[index][0] == 'id':
         id_index = index
@@ -35,19 +35,22 @@ for index in range(0, len(cursor.description)):
         long_index = index
     elif cursor.description[index][0] == 'level':
         level_index = index
+    elif cursor.description[index][0] == 'district_name':
+        district_index = index
 
 # create a table to store the pairs of schools that are less than 100 miles apart
 if connection is not None:
     try:
         cursor = connection.cursor()
         # create a list of all the school data information
-        cursor.execute("CREATE TABLE pairs_NMT_100_mi_apart("
+        cursor.execute("CREATE TABLE IF NOT EXISTS straight_line_pairs("
                        "school_1_id INTEGER NOT NULL, "
                        "school_2_id INTEGER NOT NULL, "
                        "distance_between INTEGER NOT NULL, "
-                       "PRIMARY KEY(column_1,column_2)), "
+                       "PRIMARY KEY(school_1_id, school_2_id), "
                        "FOREIGN KEY (school_1_id) REFERENCES school_info (id), "
-                       "FOREIGN KEY (school_2_id) REFERENCES school_info (id);")
+                       "FOREIGN KEY (school_2_id) REFERENCES school_info (id));")
+        connection.commit()
     except Error as e:
         print(e)
 
@@ -56,10 +59,30 @@ for school in school_list:  # for each school in the list
     for other_school in school_list:  # compare it to every othr school
         if school != other_school:  # don't compare to self:
             # if they are the same level (or in case of combos, one of the levels is the same) find the distance
-            if school[level_index] in other_school[level_index] or other_school[level_index] in school[level_index]:
-                distance_between = geopy.distance.vincenty(school, other_school).miles
+            if (school[level_index] in other_school[level_index] or other_school[level_index] in school[level_index]) \
+                    and school[district_index] != other_school[district_index]:
+                distance_between = geopy.distance.vincenty((school[lat_index], school[long_index]),
+                                                           (other_school[lat_index], other_school[long_index])).miles
                 #  if distance between is less than the threshold, save the pair
                 if distance_between <= 100:
-                    # save
+                    if connection is not None:
+                        try:
+                            cursor = connection.cursor()
+                            # save the record to the pairs table
+                            cursor.execute("INSERT INTO straight_line_pairs(school_1_id, school_2_id, "
+                                           "distance_between) VALUES ({0},{1},{2})".
+                                           format(school[id_index], other_school[id_index], distance_between))
+                            connection.commit()
+                        except Error as e:
+                            print(e)
+
     # now that we have compared to every school, remove it from the list to avoid duplicate comparisons
     school_list.remove(school)
+if connection is not None:
+    try:
+        cursor = connection.cursor()
+        # save the record to the pairs table
+        all_pairs = cursor.execute("SELECT * FROM straight_line_pairs")
+        wait = 3
+    except Error as e:
+        print(e)
