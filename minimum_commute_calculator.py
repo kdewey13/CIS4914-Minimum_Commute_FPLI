b1a6599c0f3c0f00be594c375d6a61863198f13d
kdewey13 <kdewey@ufl.edu>
2020-10-26 02:10:26 -0400

    Straight line distance works, working on optimization.

--- minimum_commute_calculator.py
+++ minimum_commute_calculator.py
@@ -2,19 +2,36 @@ import geopy.distance  # python module with function for accurate straight line 
 import pandas
 import sqlite3  # in-app database to make querying for minumums easy
 from sqlite3 import Error
+import datetime
+import config
 
 """---------------------------------------------------------------------------------"""
 """For this program to work, you must input a file in the same format as the example"""
 """---------------------------------------------------------------------------------"""
 
+"""PARAMETERS: REMOVE WHEN FUNCTION"""
+max_radius_to_consider = 50
+circuituity_factor = 2
+slow_commute_speed = 30
+fast_commute_speed = 60
+number_of_minimums_per_disct_pair = 3
+school_levels = ['elementary', 'middle', 'high']
+api_key = config.distance_key
+path_to_input_file = 'input_data.csv'
+path_to_distance_pairs_file = 'distance_pairs.csv'
+path_to_output_file = ''
+"""PARAMETERS: REMOVE WHEN FUNCTION"""
+
+
+print(datetime.datetime.now())
 # Create an in-memory database; only exists when program is running
 connection = sqlite3.connect(':memory:')
 
 # Read in the input file and put it in the db
-pandas.read_csv('input_data.csv').to_sql(name='school_info', con=connection, if_exists='replace', index=True,
-                                         index_label='id')
+pandas.read_csv(path_to_input_file).to_sql(name='school_info', con=connection, if_exists='replace', index=True,
+                                           index_label='id')
 
-# create a cursor to the
+# create a cursor to the school_info table we just created
 if connection is not None:
     try:
         cursor = connection.cursor()
@@ -41,15 +58,13 @@ for index in range(0, len(cursor.description)):
 # create a table to store the pairs of schools that are less than 100 miles apart
 if connection is not None:
     try:
-        cursor = connection.cursor()
-        # create a list of all the school data information
-        cursor.execute("CREATE TABLE IF NOT EXISTS straight_line_pairs("
-                       "school_1_id INTEGER NOT NULL, "
-                       "school_2_id INTEGER NOT NULL, "
-                       "distance_between INTEGER NOT NULL, "
-                       "PRIMARY KEY(school_1_id, school_2_id), "
-                       "FOREIGN KEY (school_1_id) REFERENCES school_info (id), "
-                       "FOREIGN KEY (school_2_id) REFERENCES school_info (id));")
+        connection.cursor().execute("CREATE TABLE IF NOT EXISTS straight_line_pairs("
+                                    "school_1_id INTEGER NOT NULL, "
+                                    "school_2_id INTEGER NOT NULL, "
+                                    "distance_between INTEGER NOT NULL, "
+                                    "PRIMARY KEY(school_1_id, school_2_id), "
+                                    "FOREIGN KEY (school_1_id) REFERENCES school_info (id), "
+                                    "FOREIGN KEY (school_2_id) REFERENCES school_info (id));")
         connection.commit()
     except Error as e:
         print(e)
@@ -64,11 +79,11 @@ for school in school_list:  # for each school in the list
                 distance_between = geopy.distance.vincenty((school[lat_index], school[long_index]),
                                                            (other_school[lat_index], other_school[long_index])).miles
                 #  if distance between is less than the threshold, save the pair
-                if distance_between <= 100:
+                if distance_between <= max_radius_to_consider:
                     if connection is not None:
                         try:
                             cursor = connection.cursor()
-                            # save the record to the pairs table
+                            # save the record to the pairs table, estimate the min and max commutes
                             cursor.execute("INSERT INTO straight_line_pairs(school_1_id, school_2_id, "
                                            "distance_between) VALUES ({0},{1},{2})".
                                            format(school[id_index], other_school[id_index], distance_between))
@@ -78,11 +93,38 @@ for school in school_list:  # for each school in the list
 
     # now that we have compared to every school, remove it from the list to avoid duplicate comparisons
     school_list.remove(school)
+print(datetime.datetime.now())
 if connection is not None:
     try:
-        cursor = connection.cursor()
-        # save the record to the pairs table
-        all_pairs = cursor.execute("SELECT * FROM straight_line_pairs")
+        # save the distance pairs for interim examining
+        pandas.DataFrame(pandas.read_sql_query("SELECT school_1_id, school_2_id, School_1_Name, "
+                                               "School_1_District, school_name as 'School_2_Name', "
+                                               "district_name as 'School_2_District', distance_between "
+                                               "FROM (SELECT school_1_id, school_2_id, school_name as 'School_1_Name', "
+                                               "district_name as 'School_1_District', distance_between "
+                                               "FROM straight_line_pairs JOIN school_info on school_1_id = id) "
+                                               "JOIN school_info on school_2_id = id", connection)).\
+            to_csv(path_to_distance_pairs_file)
         wait = 3
     except Error as e:
         print(e)
+
+# get a list of the districts
+if connection is not None:
+    try:
+        districts = list(connection.cursor().execute("SELECT DISTINCT district_name FROM school_info").fetchall())
+    except Error as e:
+        print(e)
+
+# for each district pair and each school level, narrow the list of candidates for min commute based on following:
+# find the minimum distances (as many as the desired number of minimums), take the largest of these
+# the maximum estimate for the commute time for this distance is: (circuituity_factor/slow_commute_speed)*distance
+# considering that the minimum estimated commute for any distance is: distance/fast_commute_speed
+# it is not worth considering distances where their minimum estimated commute is
+# longer than the maximum estimated commute thus, we wish to exclude distances longer than:
+# largest_min_dist*fast_commute_speed*(circuituity_factor/slow_commute_speed) from the accurate commute analysis
+
+for district in districts:
+    for other_district in districts:
+        if district != other_district:
+            for level in school_level:
