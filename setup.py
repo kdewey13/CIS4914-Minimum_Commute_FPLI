c6df001f8aa3785c307615c3feb8cb0395f754ac
kdewey13 <kdewey@ufl.edu>
2021-02-16 01:55:26 -0500

    Increment version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FPLI_Minimum_Commutes",
-    version="10",
+    version="11",
     author="Kelsey Dewey",
     author_email="fplimincomm@gmail.com",
     description="FPLI Inter-District Minimum Commute Estimator",
